ce7d9e407f66308111e87419432baf87e6cefabb
SweeterBoi <droideka.mj@gmail.com>
2023-02-28 17:49:15 +0100

    trying to read an html file into the webserver

--- webserver.py
+++ webserver.py
@@ -4,19 +4,29 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 hostName = sns.getIpAdresses()[1][:-3]
 serverPort = 80
 
+def getHtml():
+    with open('website.html', 'r') as handle:
+        lines = handle.readlines()
+    return lines
+
 class MyServer(BaseHTTPRequestHandler):
 
     def do_GET(self):
         
         whoIsHome = sns.checkWhoIsHome()
 
+        htmlFile = getHtml()
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
+        for line in htmlFile:
+            self.wfile.write(bytes(line, 'utf-8'))
+        '''
         self.wfile.write(bytes('<html><head><title>Who is home?</title></head>', 'utf-8'))
         self.wfile.write(bytes('<body>','utf-8'))
         self.wfile.write(bytes(f'<p>{whoIsHome}</p>','utf-8'))
         self.wfile.write(bytes('</body></html>','utf-8'))
+        '''
 
 
 if __name__ == '__main__':
