426f2a65004d8b736646d73ab3829a9be8c44b17
SweeterBoi <droideka.mj@gmail.com>
2023-03-05 16:14:59 +0100

    comments added

--- checkService.py
+++ checkService.py
@@ -1,25 +1,46 @@
+'''
+This script defines a continuously running service that checks the network for the
+connection status of user devices and asserts wether the usrs are at home currently or not
+the output is writeen to .bool files
+'''
 import time
 import simpleNetworkScanner as sns
 
+# Threshold for how long a user must not be connected to the network
+# for them to be considered offline
 threshold = 100 # threshold in seconds
 
+# initialise with 0 so the users are seen as last online on jan 1st 1970,
+# which is more than the threshold would allow and thus evaluating a not
+# logged in user as offline
 maxLastOnlineTime = 0.0
 alexLastOnlineTime = 0.0
 
-
+# Method calles the simpleNetworkScanner script and check who is home
+#
+# returns: a tuple of booleans
 def checkWhoIsHome():
     devices = sns.checkWhoIsHome()
+    # users are home when at least on of their recognized dvices is logged in
     maxIsHome = len(devices[0]) > 0
     alexIsHome = len(devices[1]) > 0
 
     return maxIsHome, alexIsHome
 
+# Method to write wether the user is considered online to a file
 def writeToFile(filename, value):
     with open(f'{filename}.bool', 'w') as handle:
         handle.write(str(value))
 
+# Check wether the user is offline and has not been seen for longer than the
+# threshold allowes
+#
+# parameter: name of the user and thus file to write to; no file extension!
+# parameter: value boolean indicating whether the user is considered online
+# parameter: lastOnlineTime time the user was last online
+#
+# returns: the time the user was last online to keep a rolling memory
 def parseOnlineTime(name, value, lastOnlineTime):
-    #print(f'writing {value or not abs(lastOnlineTime-time.time()) > threshold} for {name} and {abs(lastOnlineTime-time.time())}')
     if not value and abs(lastOnlineTime-time.time()) > threshold:
         writeToFile(name, False)
     elif value:
@@ -27,13 +48,14 @@ def parseOnlineTime(name, value, lastOnlineTime):
         lastOnlineTime = time.time()
     return lastOnlineTime
 
-
+# Execute this if the script is run directly
 if __name__ == '__main__':
-
+    # Run until interrupted by Ctrl+C
     try:
         while True:
-            #print('Checking Who is home...', time.time())
+            # Get the state of who is home using the checkWhoIsHome method
             maxBool, alexBool = checkWhoIsHome()
+            # Parse the last online time (this is written to the file from within the method)
             maxLastOnlineTime = parseOnlineTime('max', maxBool, maxLastOnlineTime)
             alexLastOnlineTime = parseOnlineTime('alex', alexBool, alexLastOnlineTime)
             time.sleep(10)
--- webserver.py
+++ webserver.py
@@ -1,17 +1,31 @@
+'''
+This script defines a webserver that serves a simple HTML page
+showing wehter Alex or Maexchen were logged into the home wifi
+within the last 5 minutes
+'''
 import simpleNetworkScanner as sns
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+# Get own IP address via simpleNetworkScanner method to use as IP for the 
+# Webserver
 hostName = sns.getIpAdresses()[1][:-3]
+# Port of the webserver
 serverPort = 80
-insertLine = 45
 
+# This line of html is inserted into the webpage to gray out namecard of 
+# Alex or Maexchen if they are not at home
 darkClass = '<div class="darkClass"><p class="notHome">Nicht zu Hause</p></div>'
 
+# open the htmp file and return it as a list of lines
 def getHtml():
     with open('website.html', 'r') as handle:
         lines = handle.readlines()
     return lines
 
+# check for savefiles which contain wether the specified person is logged
+# as at home or not
+#
+# returns: a tuple of booleans each True if the person is at home, False if not
 def checkHomeFiles():
     with open('max.bool', 'r') as handle:
         maxOnline = handle.readline().startswith('True')
@@ -19,36 +33,49 @@ def checkHomeFiles():
         alexOnline = handle.readline().startswith('True')
     return maxOnline, alexOnline
         
-
+# Class defines a webserver that serves a simple HTML page and manipulates the
+# base webpage to show wehter Alex or Maexchen are logged into the home wifi
 class MyServer(BaseHTTPRequestHandler):
 
+    # This method is called when a GET request is made to the server by a client
     def do_GET(self):
+        # Calls the function above to check who is home
         maxOnline, alexOnline = checkHomeFiles()
+        # Call the function to get the html file
         htmlFile = getHtml()
+        # Send a 'OK' response to the client
         self.send_response(200)
+        # Send the Header of the server response
         self.send_header('Content-type', 'text/html')
         self.end_headers()
+        # Iteraates over the lines of the html file, inserting the darkClass line
+        # if the person is not at home
         for index, line in enumerate(htmlFile):
-
+            # Check for Maexchen
             if line.startswith('<!--Add darkClass, Max-->'):
                 if not maxOnline:
+                    # Insert the darkClass line instead of the comment line
                     self.wfile.write(bytes(darkClass, 'utf-8'))
                 continue
+            # Also check for Alex
             elif line.startswith('<!--Add darkClass, Alex-->'):
                 if not alexOnline:
+                    # Insert the darkClass line instead of the comment line
                     self.wfile.write(bytes(darkClass, 'utf-8'))
                 continue
 
             self.wfile.write(bytes(line, 'utf-8'))
 
-
+# Execute this if the script is called directly
 if __name__ == '__main__':
+    # Short console output
     print(f'Serving at {hostName}:{serverPort}')
+    # Create the webserver
     webServer = HTTPServer((hostName, serverPort), MyServer)
-
+    # Serve until console interrupt happens
     try:
         webServer.serve_forever()
     except KeyboardInterrupt:
         pass
-
+    # close the webserver
     webServer.server_close()
